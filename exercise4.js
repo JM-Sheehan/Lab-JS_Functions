6b869603e24da579e8de35853101bdd5153a6e16
Jamie Sheehan <jamie.michael.sheehan@gmail.com>
2021-07-30 22:06:08 +0100

    Exercise 4

--- exercise4.js
+++ exercise4.js
@@ -1,9 +1,11 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
-    const uncompleted = json.filter((todo) => !todo.completed).
-    map(todo=>({userId: todo.userId, title: todo.title}))
-    console.log(uncompleted);
+     const totalCompleted = json.reduce( (acc, todo) => 
+          todo.completed ? acc+1 : acc
+     ,0
+     )
+     console.log(totalCompleted)
   })
   .catch(function(err) { 
     console.log(err);
